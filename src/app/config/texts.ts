a67067f43799a3062bda73b66765a52518e6d1d5
NoemiLengyel <lengyel.noemi96@gmail.com>
2025-10-07 00:42:15 +0200

    add greeting text

--- src/app/config/texts.ts
+++ src/app/config/texts.ts
@@ -4,13 +4,26 @@ import { ITextConfig } from '../models/types';
 export const TEXT_CONFIGS: ITextConfig[] = [
   {
     key: KEY.text.prompt,
-    position: {x: 200, y: 200},
+    position: { x: 200, y: 200 },
     text: 'Press [E] to interact',
     style: {
       fontSize: '16px',
       color: '#d3ff11ff',
       backgroundColor: '#051a17ff',
       padding: { x: 8, y: 4 },
+    },
+  },
+  {
+    key: 'greeting',
+    position: { x: 365, y: 280 },
+    text: 'Greetings, visitor! It seems like the forest animals took my resumé, would you help me find it? [Press any arrow key to continue...]',
+    style: {
+      fontSize: '16px',
+      color: '#f0f0f0ff',
+      backgroundColor: '#494949c7',
+      wordWrap: { width: 200 },
+      padding: { x: 8, y: 4 },
+      align: 'center',
     },
   },
 ];
